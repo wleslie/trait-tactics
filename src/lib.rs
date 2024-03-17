251e3b68340aff89b610c9a23d97d5069bd401d1
William J. Leslie <will@willleslie.com>
2024-03-17 15:16:43 -0700

    Factor out impl-trait head parsing

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 
 use itertools::Itertools;
 use proc_macro::TokenStream;
-use quote::quote;
+use quote::{quote, ToTokens};
 use syn::{
     braced,
     parse::{Parse, ParseStream},
@@ -14,24 +14,17 @@ use syn::{
 #[proc_macro]
 pub fn assign_via_binop_ref(input: TokenStream) -> TokenStream {
     struct Args {
-        impl_tok: Token![impl],
-        generics: Generics,
-        tr: Path,
+        head: ImplTraitHead,
         rhs: Type,
-        for_tok: Token![for],
-        self_ty: Type,
         fn_tok: Token![fn],
         method: Ident,
         delegate: Path,
     }
     impl Parse for Args {
         fn parse(input: ParseStream) -> syn::Result<Self> {
-            let impl_tok = input.parse()?;
-            let mut generics =
-                if input.peek(Token![<]) { input.parse()? } else { Generics::default() };
-            let tr = input.parse::<Path>()?;
+            let head = input.parse::<ImplTraitHead>()?;
             let rhs = if let PathArguments::AngleBracketed(list) =
-                &tr.segments.last().expect("path cannot be empty").arguments
+                &head.tr.segments.last().expect("path cannot be empty").arguments
             {
                 list.args.iter().exactly_one().ok().map(|arg| {
                     if let GenericArgument::Type(ty) = arg {
@@ -44,31 +37,52 @@ pub fn assign_via_binop_ref(input: TokenStream) -> TokenStream {
                 None
             }
             .unwrap_or_else(|| {
-                Err(syn::Error::new(tr.span(), "expected exactly one generic argument"))
+                Err(syn::Error::new(head.tr.span(), "expected exactly one generic argument"))
             })?
             .clone();
-            let for_tok = input.parse()?;
-            let self_ty = input.parse()?;
-            generics.where_clause = input.parse()?;
             let content;
             braced!(content in input);
             let fn_tok = content.parse()?;
             let method = content.parse()?;
             content.parse::<Token![=>]>()?;
             let delegate = content.parse()?;
-            Ok(Self { impl_tok, generics, tr, rhs, for_tok, self_ty, fn_tok, method, delegate })
+            Ok(Self { head, rhs, fn_tok, method, delegate })
         }
     }
-    let Args { impl_tok, generics, tr, rhs, for_tok, self_ty, fn_tok, method, delegate, .. } =
-        parse_macro_input!(input);
-    let where_clause = &generics.where_clause;
+    let Args { head, rhs, fn_tok, method, delegate, .. } = parse_macro_input!(input);
 
     quote! {
-        #impl_tok #generics #tr #for_tok #self_ty #where_clause {
+        #head {
             #fn_tok #method(&mut self, rhs: #rhs) {
                 *self = #delegate(&*self, rhs);
             }
         }
     }
     .into()
+}
+
+struct ImplTraitHead {
+    impl_tok: Token![impl],
+    generics: Generics,
+    tr: Path,
+    for_tok: Token![for],
+    self_ty: Type,
+}
+impl Parse for ImplTraitHead {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let impl_tok = input.parse()?;
+        let mut generics = if input.peek(Token![<]) { input.parse()? } else { Generics::default() };
+        let tr = input.parse::<Path>()?;
+        let for_tok = input.parse()?;
+        let self_ty = input.parse()?;
+        generics.where_clause = input.parse()?;
+        Ok(Self { impl_tok, generics, tr, for_tok, self_ty })
+    }
+}
+impl ToTokens for ImplTraitHead {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        let Self { impl_tok, generics, tr, for_tok, self_ty } = self;
+        let where_clause = &generics.where_clause;
+        tokens.extend(quote!(#impl_tok #generics #tr #for_tok #self_ty #where_clause));
+    }
 }
